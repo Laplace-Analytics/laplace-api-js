52aa56f380531c64f7a3fefcc6759e1d9c09ce5e
Kerem Balic <92550348+KereMBC@users.noreply.github.com>
2025-06-24 15:03:14 +0300

    #77006 feat(test): update collection tests

--- src/test/collections.test.ts
+++ src/test/collections.test.ts
@@ -1,9 +1,10 @@
-import { Logger } from 'winston';
-import { LaplaceConfiguration } from '../utilities/configuration';
-import { CollectionClient, Region, Locale } from '../client/collections';
-import './client_test_suite';
+import { Logger } from "winston";
+import { LaplaceConfiguration } from "../utilities/configuration";
+import { CollectionClient, Locale, Region } from "../client/collections";
+import "./client_test_suite";
+import { validateCollection, validateCollectionDetail } from "./helpers";
 
-describe('Collections', () => {
+describe("Collections", () => {
   let client: CollectionClient;
 
   beforeAll(() => {
@@ -19,18 +20,75 @@ describe('Collections', () => {
     client = new CollectionClient(config, logger);
   });
 
-  test('GetAllIndustries', async () => {
+  test("GetAllIndustries", async () => {
     const resp = await client.getAllIndustries(Region.Tr, Locale.Tr);
     expect(resp).not.toBeEmpty();
+
+    const firstIndustry = resp[0];
+    validateCollection(firstIndustry);
   });
 
-  test('GetIndustryDetails', async () => {
-    const resp = await client.getIndustryDetail("65533e441fa5c7b58afa0944", Region.Tr, Locale.Tr);
+  test("GetAllSectors", async () => {
+    const resp = await client.getAllSectors(Region.Tr, Locale.Tr);
     expect(resp).not.toBeEmpty();
+
+    const firstSector = resp[0];
+    validateCollection(firstSector);
   });
 
-  test('GetSectorDetails', async () => {
-    const resp = await client.getSectorDetail("65533e047844ee7afe9941b9", Region.Tr, Locale.Tr);
+  test("GetIndustryDetails", async () => {
+    const resp = await client.getIndustryDetail(
+      "65533e441fa5c7b58afa0944",
+      Region.Tr,
+      Locale.Tr
+    );
     expect(resp).not.toBeEmpty();
+    validateCollectionDetail(resp);
+  });
+
+  test("GetSectorDetails", async () => {
+    const resp = await client.getSectorDetail(
+      "65533e047844ee7afe9941b9",
+      Region.Tr,
+      Locale.Tr
+    );
+    expect(resp).not.toBeEmpty();
+    validateCollectionDetail(resp);
+  });
+
+  test("GetAllThemes", async () => {
+    const resp = await client.getAllThemes(Region.Tr, Locale.Tr);
+    expect(resp).not.toBeEmpty();
+
+    const firstTheme = resp[0];
+    validateCollection(firstTheme);
+  });
+
+  test("GetThemeDetails", async () => {
+    const resp = await client.getThemeDetail(
+      "6256b0647d0bb100123effa7",
+      Region.Tr,
+      Locale.Tr
+    );
+    expect(resp).not.toBeEmpty();
+    validateCollectionDetail(resp);
+  });
+
+  test("GetAllCollections", async () => {
+    const resp = await client.getAllCollections(Region.Tr, Locale.Tr);
+    expect(resp).not.toBeEmpty();
+
+    const firstCollection = resp[0];
+    validateCollection(firstCollection);
+  });
+
+  test("GetCollectionDetails", async () => {
+    const resp = await client.getThemeDetail(
+      "620f455a0187ade00bb0d55f",
+      Region.Tr,
+      Locale.Tr
+    );
+    expect(resp).not.toBeEmpty();
+    validateCollectionDetail(resp);
   });
 });
