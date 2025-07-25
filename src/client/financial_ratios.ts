ba9c5884e28ae6d46a3e7564169849c612a6cf73
Fatih Emin Öge <fatih@finfree.co>
2025-07-25 18:18:52 +0600

    switch to v3

--- src/client/financial_ratios.ts
+++ src/client/financial_ratios.ts
@@ -1,9 +1,9 @@
-import { Client } from './client';
-import { Region, Locale } from './collections';
+import { Client } from "./client";
+import { Region, Locale } from "./collections";
 
 export enum RatioComparisonPeerType {
-  Industry = 'industry',
-  Sector = 'sector'
+  Industry = "industry",
+  Sector = "sector",
 }
 
 export interface StockPeerFinancialRatioComparison {
@@ -37,85 +37,85 @@ export interface StockHistoricalRatiosData {
 }
 
 export enum HistoricalRatiosFormat {
-  CURRENCY = 'currency',
-  PERCENTAGE = 'percentage',
-  DECIMAL = 'decimal',
+  CURRENCY = "currency",
+  PERCENTAGE = "percentage",
+  DECIMAL = "decimal",
 }
 
 export enum HistoricalRatiosKey {
-  Revenue = 'satislar',
-  EBITDA = 'ebitda',
-  NetProfit = 'net_kar',
-  GrossMargin = 'gross-margin',
-  NetMargin = 'net-margin',
-  ReturnOnAssets = 'roa',
-  ReturnOnEquity = 'roe',
-  ReturnOnCapitalEmployed = 'roce',
-  ReturnOnInvestedCapital = 'roic',
-  PriceToEarningsRatio = 'pe-ratio',
-  PriceToEarnings = 'poe',
-  PriceToBookRatio = 'pb-ratio',
-  EnterpriseValueToEBITDA = 'ev-to-ebitda',
-  EnterpriseValueToInvestedCapital = 'evic',
-  InterestCoverage = 'interestCoverage',
-  QuickRatio = 'quick-ratio',
-  LeverageRatio = 'leverage-ratio',
-  DebtToEquity = 'debt-to-equity',
-  RevenueGrowth = 'satis_buyumesi',
-  EBITDAGrowth = 'favok_buyumesi',
-  NetProfitGrowth = 'net_kar_buyumesi',
-  FreeCashFlowGrowth = 'serbest_nakit_akisi_buyumesi',
-  CashConversionCycle = 'cash-conversion-cycle',
-  DaysSalesOutstanding = 'days-sales-outstanding',
-  DaysPayable = 'days-payable',
-  DaysInventory = 'days-inventory',
+  Revenue = "satislar",
+  EBITDA = "ebitda",
+  NetProfit = "net_kar",
+  GrossMargin = "gross-margin",
+  NetMargin = "net-margin",
+  ReturnOnAssets = "roa",
+  ReturnOnEquity = "roe",
+  ReturnOnCapitalEmployed = "roce",
+  ReturnOnInvestedCapital = "roic",
+  PriceToEarningsRatio = "pe-ratio",
+  PriceToEarnings = "poe",
+  PriceToBookRatio = "pb-ratio",
+  EnterpriseValueToEBITDA = "ev-to-ebitda",
+  EnterpriseValueToInvestedCapital = "evic",
+  InterestCoverage = "interestCoverage",
+  QuickRatio = "quick-ratio",
+  LeverageRatio = "leverage-ratio",
+  DebtToEquity = "debt-to-equity",
+  RevenueGrowth = "satis_buyumesi",
+  EBITDAGrowth = "favok_buyumesi",
+  NetProfitGrowth = "net_kar_buyumesi",
+  FreeCashFlowGrowth = "serbest_nakit_akisi_buyumesi",
+  CashConversionCycle = "cash-conversion-cycle",
+  DaysSalesOutstanding = "days-sales-outstanding",
+  DaysPayable = "days-payable",
+  DaysInventory = "days-inventory",
 
-  EBITDAMargin = 'favok_marji',
-  InventoryTurnover = 'inventory-turnover',
-  DepositGrowth = 'mevduat_buyumesi',
-  NetInterestMargin = 'net_faiz_marji',
-  CompensationGrowth = 'gerceklesen_tazminatlar_buyumesi',
-  PremiumPerCompensation = 'prim_basina_tazminat_orani',
-  EnterpriseValueToOperatingCashFlow = 'evOcf',
-  EarningsBeforeTax = 'ebt',
-  CapitalExpenditure = 'capex',
-  FinancialInvestments = 'financial_investments',
-  RealtimeEarningsPerShare = 'realtime_eps-basic',
-  RealtimeMarketValue = 'realtime_piyasa_degeri',
-  RealtimePriceToBookRatio = 'realtime_pb-ratio',
-  RealtimePriceToEarningsRatio = 'realtime_pe-ratio',
-  CurrentRatio = 'current-ratio',
-  AssetTurnover = 'asset-turnover',
-  TotalOperationalExpense = 'total_operational_expense',
-  TotalOperationalExpenseToGrossProfit = 'total_operational_expense_gross_profit_ratio',
-  CashAndCashEquivalents = 'cash_and_cash_equivalents',
-  CashToAssets = 'cash_to_assets',
-  CapexToNetProfit = 'capex_to_net_profit',
-  RealtimeEnterpriseValueToEBITDA = 'realtime_ev-to-ebitda',
-  ReceivablesTurnover = 'alacak_devir_hizi',
-  EarningsPerShare = 'eps-basic',
-  CreditToAssetRatio = 'kredi_aktif_orani',
-  CreditToDepositRatio = 'kredi_mevduat_orani',
-  TechnicalProfitGrowth = 'teknik_kar_buyumesi',
-  NetEarnedPremiumGrowth = 'net_kazanilan_prim_buyumesi',
-  EBITGrowth = 'ebitGrowth',
-  CashReturnOnInvestedCapital = 'croic',
-  MarketCapitalization = 'piyasa_degeri',
-  ShortTermToLongTermObligations = 'short_term_obligations_long_term_obligations',
-  RetainedEarnings = 'retained_earnings',
-  ThreeYearCAGRFreeCashFlow = 'three_year_cagr_free_cash_flow',
-  LongTermLoansToPeriodicProfitRatio = 'long_term_loans_period_profit_ratio',
-  LongTermLoans = 'long_term_loans',
-  CommercialReceivablesToTotalCurrentAssets = 'commercial_receivables_total_current_assets',
-  StockGrowth = 'stock_growth',
-  FiveYearRetainedEarningsChange = 'five_year_retained_earnings_change',
-  ThreeYearCAGRRetainedEarnings = 'three_year_cagr_retained_earnings',
-  PotentialOperatingCashFlow = 'pocf',
-  FreeCashFlowToEnterpriseValue = 'fcfEv',
-  DebtToDeposit = 'dd',
-  NetDebt = 'net_borc',
-  PaidCapital = 'odenmis_sermaye',
-  FinancialExpensesToEBIT = 'financial_expenses_ebit_ratio'
+  EBITDAMargin = "favok_marji",
+  InventoryTurnover = "inventory-turnover",
+  DepositGrowth = "mevduat_buyumesi",
+  NetInterestMargin = "net_faiz_marji",
+  CompensationGrowth = "gerceklesen_tazminatlar_buyumesi",
+  PremiumPerCompensation = "prim_basina_tazminat_orani",
+  EnterpriseValueToOperatingCashFlow = "evOcf",
+  EarningsBeforeTax = "ebt",
+  CapitalExpenditure = "capex",
+  FinancialInvestments = "financial_investments",
+  RealtimeEarningsPerShare = "realtime_eps-basic",
+  RealtimeMarketValue = "realtime_piyasa_degeri",
+  RealtimePriceToBookRatio = "realtime_pb-ratio",
+  RealtimePriceToEarningsRatio = "realtime_pe-ratio",
+  CurrentRatio = "current-ratio",
+  AssetTurnover = "asset-turnover",
+  TotalOperationalExpense = "total_operational_expense",
+  TotalOperationalExpenseToGrossProfit = "total_operational_expense_gross_profit_ratio",
+  CashAndCashEquivalents = "cash_and_cash_equivalents",
+  CashToAssets = "cash_to_assets",
+  CapexToNetProfit = "capex_to_net_profit",
+  RealtimeEnterpriseValueToEBITDA = "realtime_ev-to-ebitda",
+  ReceivablesTurnover = "alacak_devir_hizi",
+  EarningsPerShare = "eps-basic",
+  CreditToAssetRatio = "kredi_aktif_orani",
+  CreditToDepositRatio = "kredi_mevduat_orani",
+  TechnicalProfitGrowth = "teknik_kar_buyumesi",
+  NetEarnedPremiumGrowth = "net_kazanilan_prim_buyumesi",
+  EBITGrowth = "ebitGrowth",
+  CashReturnOnInvestedCapital = "croic",
+  MarketCapitalization = "piyasa_degeri",
+  ShortTermToLongTermObligations = "short_term_obligations_long_term_obligations",
+  RetainedEarnings = "retained_earnings",
+  ThreeYearCAGRFreeCashFlow = "three_year_cagr_free_cash_flow",
+  LongTermLoansToPeriodicProfitRatio = "long_term_loans_period_profit_ratio",
+  LongTermLoans = "long_term_loans",
+  CommercialReceivablesToTotalCurrentAssets = "commercial_receivables_total_current_assets",
+  StockGrowth = "stock_growth",
+  FiveYearRetainedEarningsChange = "five_year_retained_earnings_change",
+  ThreeYearCAGRRetainedEarnings = "three_year_cagr_retained_earnings",
+  PotentialOperatingCashFlow = "pocf",
+  FreeCashFlowToEnterpriseValue = "fcfEv",
+  DebtToDeposit = "dd",
+  NetDebt = "net_borc",
+  PaidCapital = "odenmis_sermaye",
+  FinancialExpensesToEBIT = "financial_expenses_ebit_ratio",
 }
 
 export interface StockHistoricalRatiosDescription {
@@ -148,21 +148,21 @@ export interface HistoricalFinancialSheetRow {
 }
 
 export enum FinancialSheetType {
-  IncomeStatement = 'incomeStatement',
-  BalanceSheet = 'balanceSheet',
-  CashFlow = 'cashFlowStatement',
+  IncomeStatement = "incomeStatement",
+  BalanceSheet = "balanceSheet",
+  CashFlow = "cashFlowStatement",
 }
 
 export enum FinancialSheetPeriod {
-  Annual = 'annual',
-  Quarterly = 'quarterly',
-  Cumulative = 'cumulative',
+  Annual = "annual",
+  Quarterly = "quarterly",
+  Cumulative = "cumulative",
 }
 
 export enum Currency {
-  USD = 'USD',
-  TRY = 'TRY',
-  EUR = 'EUR',
+  USD = "USD",
+  TRY = "TRY",
+  EUR = "EUR",
 }
 
 export interface FinancialSheetDate {
@@ -178,14 +178,14 @@ export class FinancialClient extends Client {
     peerType: RatioComparisonPeerType
   ): Promise<StockPeerFinancialRatioComparison[]> {
     const url = new URL(
-      `${this['baseUrl']}/api/v2/stock/financial-ratio-comparison`
+      `${this["baseUrl"]}/api/v2/stock/financial-ratio-comparison`
     );
-    url.searchParams.append('symbol', symbol);
-    url.searchParams.append('region', region);
-    url.searchParams.append('peerType', peerType);
+    url.searchParams.append("symbol", symbol);
+    url.searchParams.append("region", region);
+    url.searchParams.append("peerType", peerType);
 
     return this.sendRequest<StockPeerFinancialRatioComparison[]>({
-      method: 'GET',
+      method: "GET",
       url: url.toString(),
     });
   }
@@ -196,14 +196,14 @@ export class FinancialClient extends Client {
     region: Region,
     locale: Locale
   ): Promise<StockHistoricalRatios[]> {
-    const url = new URL(`${this['baseUrl']}/api/v2/stock/historical-ratios`);
-    url.searchParams.append('symbol', symbol);
-    url.searchParams.append('region', region);
-    url.searchParams.append('locale', locale);
-    url.searchParams.append('slugs', keys.join(','));
+    const url = new URL(`${this["baseUrl"]}/api/v2/stock/historical-ratios`);
+    url.searchParams.append("symbol", symbol);
+    url.searchParams.append("region", region);
+    url.searchParams.append("locale", locale);
+    url.searchParams.append("slugs", keys.join(","));
 
     return this.sendRequest<StockHistoricalRatios[]>({
-      method: 'GET',
+      method: "GET",
       url: url.toString(),
     });
   }
@@ -213,13 +213,13 @@ export class FinancialClient extends Client {
     region: Region
   ): Promise<StockHistoricalRatiosDescription[]> {
     const url = new URL(
-      `${this['baseUrl']}/api/v2/stock/historical-ratios/descriptions`
+      `${this["baseUrl"]}/api/v2/stock/historical-ratios/descriptions`
     );
-    url.searchParams.append('locale', locale);
-    url.searchParams.append('region', region);
+    url.searchParams.append("locale", locale);
+    url.searchParams.append("region", region);
 
     return this.sendRequest<StockHistoricalRatiosDescription[]>({
-      method: 'GET',
+      method: "GET",
       url: url.toString(),
     });
   }
@@ -234,35 +234,35 @@ export class FinancialClient extends Client {
     region: Region
   ): Promise<HistoricalFinancialSheets> {
     const url = new URL(
-      `${this['baseUrl']}/api/v2/stock/historical-financial-sheets`
+      `${this["baseUrl"]}/api/v3/stock/historical-financial-sheets`
     );
-    url.searchParams.append('symbol', symbol);
+    url.searchParams.append("symbol", symbol);
     url.searchParams.append(
-      'from',
-      `${from.year.toString().padStart(4, '0')}-${from.month
+      "from",
+      `${from.year.toString().padStart(4, "0")}-${from.month
         .toString()
-        .padStart(2, '0')}-${from.day.toString().padStart(2, '0')}`
+        .padStart(2, "0")}-${from.day.toString().padStart(2, "0")}`
     );
     url.searchParams.append(
-      'to',
-      `${to.year.toString().padStart(4, '0')}-${to.month
+      "to",
+      `${to.year.toString().padStart(4, "0")}-${to.month
         .toString()
-        .padStart(2, '0')}-${to.day.toString().padStart(2, '0')}`
+        .padStart(2, "0")}-${to.day.toString().padStart(2, "0")}`
     );
-    url.searchParams.append('sheetType', sheetType);
-    url.searchParams.append('periodType', period);
-    url.searchParams.append('currency', currency);
-    url.searchParams.append('region', region);
+    url.searchParams.append("sheetType", sheetType);
+    url.searchParams.append("periodType", period);
+    url.searchParams.append("currency", currency);
+    url.searchParams.append("region", region);
 
     if (
       sheetType === FinancialSheetType.BalanceSheet &&
       period !== FinancialSheetPeriod.Cumulative
     ) {
-      throw new Error('balance sheet is only available for cumulative period');
+      throw new Error("balance sheet is only available for cumulative period");
     }
 
     return this.sendRequest<HistoricalFinancialSheets>({
-      method: 'GET',
+      method: "GET",
       url: url.toString(),
     });
   }
