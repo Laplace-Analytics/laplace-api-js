98cacb49d595d9635800c027cdc0a06c074f108d
Kerem Balic <92550348+KereMBC@users.noreply.github.com>
2026-06-18 16:08:13 +0300

    publish compiled JS + types with public API barrel

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// Base client and configuration
+export * from './client/client';
+export * from './utilities/configuration';
+
+// Errors
+export * from './client/errors';
+
+// Core market data clients and their types
+export * from './client/stocks';
+export * from './client/financial_fundamentals';
+export * from './client/collections';
+
+// Additional clients
+export * from './client/search';
+export * from './client/news';
+export * from './client/broker';
+export * from './client/live-price';
+export * from './client/custom_theme';
+export * from './client/financial_ratios';
+export * from './client/capital_increase';
+export * from './client/screener';
+export * from './client/funds';
+export * from './client/key-insights';
+export * from './client/politician';
+export * from './client/live-price-web-socket';
